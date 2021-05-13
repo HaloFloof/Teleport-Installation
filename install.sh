7518020eed76509bed15a612008a0bb7dbcd9136
HaloFloof <65753577+HaloFloof@users.noreply.github.com>
2021-05-12 22:42:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
+ 
 lsb_dist="$(. /etc/os-release && echo "$ID")"
 dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-if [ "$lsb_dist" =  "centos" ]
- then
+if [ "$lsb_dist" =  "centos" ]; then
 	yum-config-manager --add-repo https://rpm.releases.teleport.dev/teleport.repo
 	yum install teleport -y
-elif [ "$lsb_dist" =  "ubuntu" ]
-  then
+elif [ "$lsb_dist" =  "ubuntu" ]; then
     curl https://deb.releases.teleport.dev/teleport-pubkey.asc | sudo apt-key add -
 	add-apt-repository 'deb https://deb.releases.teleport.dev/ stable main'
 	apt-get update -y
